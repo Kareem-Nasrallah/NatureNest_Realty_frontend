87107e846447d7d2bf4bd23f3b4918578733b0c1
Kareem Nasrallah <kareemnasrallah88@gmail.com>
2024-10-10 16:26:53 +0300

    some edites

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'NatureNest_Realty_frontend',
+  base: 'naturenest_realty_frontend',
 })
